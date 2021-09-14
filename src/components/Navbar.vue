0c63acd6dc6b132e8fa72fb547bd31dbaecd8fa8
Dicky Jiang <dickyjuwono@gmail.com>
2021-09-14 20:48:19 +0700

    wip

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="fixed flex justify-between w-full bg-white text-lg font-bold px-3 py-5 border-b-2">
+    <div class="flex gap-3">
+        <div>
+            logo
+        </div>
+        <div>
+            RMA Sejahtera Mandiri Perkasa
+        </div>
+    </div>
+    <div class="flex gap-3">
+        <div>
+            Kirim Garansi
+        </div>
+        <div>
+            Cek Status
+        </div>
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar />
+  <div class="bg-gray-300 h-screen min-h-screen pt-28">
+    <div class="flex justify-center items-center bg-red-500 max-w-3xl mx-auto">
+      <h1 class="text-center font-bold text-2xl">Input Serial Number </h1>
+
+      <form class="appearance-none mt-12">
+        <input type="text">
+      </form>
+    </div>
   </div>
+ 
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '../components/Navbar.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
