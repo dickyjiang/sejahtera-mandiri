69bd2cb21bb1d016925cff27ffd8ccfdef406ae8
Dicky Jiang <dickyjuwono@gmail.com>
2021-09-15 01:13:19 +0700

    masukin table v-for

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,39 @@
 <template>
   <Navbar />
-  <div class="bg-gray-300 h-screen min-h-screen pt-28">
-    <div class="flex justify-center items-center bg-red-500 max-w-3xl mx-auto">
+  <div class="bg-gray-50 h-screen min-h-screen pt-28">
+    <div class="flex flex-col bg-yellow-400 justify-center items-center  max-w-3xl mx-auto">
       <h1 class="text-center font-bold text-2xl">Input Serial Number </h1>
+      <div class="max-w-3xl w-3/4">
+        <form class="mt-12 flex flex-col gap-4">
+          <input class="bg-white border px-3 text-gray-300 py-2" type="text" placeholder="Serial Number">
+          <button class="px-3 py-2 w-32 text-white font-semibold shadow-md  rounded bg-turkis">Tambah</button>
+        </form>
+      </div>
+      <div class="mt-12 bg-gray-100">
 
-      <form class="appearance-none mt-12">
-        <input type="text">
-      </form>
-    </div>
+        <table class="table table-striped table-bordered border-collapse border-2 border-gray-500">
+            <thead>
+                <tr>
+                    <th class="w-1/3">Serial Number</th>
+                    <th class="w-1/4">Kode Barang</th>
+                    <th class="w-1/4">Nama Barang</th>
+                    <th class="w-1/4">Masa Garansi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in items" :key="item.id">
+                    <td>{{item.serialNumber}}</td>
+                    <td>{{item.kodeBarang}}</td>
+                    <td>{{item.namaBarang}}</td>
+                    <td>{{item.masaGaransi}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>    
+
   </div>
+</div>
+
  
 </template>
 
@@ -17,8 +42,18 @@ import Navbar from '../components/Navbar.vue'
 
 export default {
   name: 'Home',
-  components: {
-    Navbar
-  }
+  components: {Navbar},
+
+  data() {
+        return {
+            items: [
+                { serialNumber: '123456789', kodeBarang: 'SM-PR42', namaBarang: '3MP WIFI NETWORK CAMERA', masaGaransi: '21 Desember 2022' },
+                { serialNumber: '123456789', kodeBarang: 'SM-PR42', namaBarang: '3MP WIFI NETWORK CAMERA', masaGaransi: '21 Desember 2022' },
+                { serialNumber: '123456789', kodeBarang: 'SM-PR42', namaBarang: '3MP WIFI NETWORK CAMERA', masaGaransi: '21 Desember 2022' },
+                { serialNumber: '123456789', kodeBarang: 'SM-PR42', namaBarang: '3MP WIFI NETWORK CAMERA', masaGaransi: '21 Desember 2022' }
+            ]
+        };
+    }
+
 }
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        turkis: '#40b0bf',
+      },
+    },
   },
   variants: {
     extend: {},
